94288822d37f00d614716e676b6ac862fb82b205
tukangzheng <tukangzheng@gomeplus.com>
2017-10-11 16:51:35 +0800

    开发新功能

--- gateserver/src/main/java/com/cncnc/gateserver/handler/GateServerHandler.java
+++ gateserver/src/main/java/com/cncnc/gateserver/handler/GateServerHandler.java
@@ -1,10 +1,15 @@
 package com.cncnc.gateserver.handler;
 
+import com.cncnc.gateserver.ClientMessage;
 import com.cncnc.gateserver.utils.ClientConnection;
 import com.cncnc.gateserver.utils.ClientConnectionMap;
 import com.google.protobuf.Message;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
 
 public class GateServerHandler extends SimpleChannelInboundHandler<Message> {
 
@@ -15,7 +20,8 @@ public class GateServerHandler extends SimpleChannelInboundHandler<Message> {
      */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-
+        // 保存客户端的连接   方便以后去做处理
+        ClientConnectionMap.addClientConnetion(ctx);
     }
 
     /**
@@ -27,15 +33,20 @@ public class GateServerHandler extends SimpleChannelInboundHandler<Message> {
      * 2、ChannelRead0 是SimpleChannelInboundHandler这个类里面的方法，每次调用之后不需要去手动释放资源，
      * 内部会自动去释放资源（也是通过调用ReferenceCountUtil.release(msg)去释放资源）
      *
-     * @param channelHandlerContext
+     * @param ctx
      * @param message
      * @throws Exception
      */
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Message message) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, Message message) throws Exception {
         // 获取客户端的连接
-        ClientConnection conn = ClientConnectionMap.getClientConnection(channelHandlerContext);
+        ClientConnection conn = ClientConnectionMap.getClientConnection(ctx);
+        ClientMessage.processTransferHandler(message, conn);
 
+        // 通知客户端已经收到消息
+        String responseMessage = "Hello, I have received the message!!";
+        ByteBuf byteBuf = Unpooled.copiedBuffer(responseMessage, CharsetUtil.UTF_8);
+        ctx.writeAndFlush(byteBuf);
     }
 
     /**
